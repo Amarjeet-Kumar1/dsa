bb32892e3f92b3e82648091b31e0ba67ff0851f4
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-11-05 22:07:43 +0530

    insert an element in a array

--- array/insert.cpp
+++ array/insert.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+int insertEle(int arr[], int n, int x, int pos, int cap){
+    if(n == cap)
+        return n;
+    int idx = pos-1;
+    for(int i=n-1;i>=idx;i--){
+        arr[i+1]= arr[i];
+    }
+    arr[idx] = x;
+    return n+1;
+    }
 
+int main(){
+    int x = 15, pos = 3, cap = 5;
+    int arr[cap] = {10, 20, 5, 6};
+    int n = 4;
+    int insert = insertEle(arr, n, x, pos, cap);
+    if(insert == n+1)
+    {
+    cout<<x<<" is inserted at position: "<<pos<<endl;
+    cout<<"new array is: ";
+    for(int i =0; i<n+1;i++){
+        cout<<arr[i]<<" ";
+    }
+    }
+    else if(insert == n){
+        cout<<"Array is full.";
+    }
+    return 0;
+}
