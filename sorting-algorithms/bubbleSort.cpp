57ee93930d26c11d191f5edcb70b3f0c20763dfa
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-11-23 15:56:15 +0530

    optimized for already swapped array

--- sorting-algorithms/bubbleSort.cpp
+++ sorting-algorithms/bubbleSort.cpp
@@ -3,10 +3,17 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n){
-    for(int i = 0; i< n; i++)
-        for(int j = 0; j<n-i-1; j++)
-            if(arr[j]>arr[j+1])
+    for(int i = 0; i< n; i++){
+        bool swapped = false;
+        for(int j = 0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+            }
+        if(swapped == false)
+            break;
+    }            
 }
 int main(){
     int arr[] = {2, 5, 1, 15, 4};
