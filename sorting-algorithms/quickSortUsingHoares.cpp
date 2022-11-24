2508a9c6d380f28670cb340b04e9c00ba568ce76
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-11-24 23:36:15 +0530

    quick sort using hoare's algorithm

---
+++ sorting-algorithms/quickSortUsingHoares.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+//Hoare's partition algorithm
+//it consider first element as pivot
+
+int partition(int arr[], int l, int h){
+    int pivot = arr[l];
+    int i = l-1, j = h+1;
+    while(true){
+        do{
+            i++;
+        } while(arr[i] < pivot);
+        do{
+            j--;
+        } while(arr[j] > pivot);
+        if(i >= j) return j;
+        swap(arr[i], arr[j]);
+        
+    }
+}
+
+//quick sort
+
+void quickSort(int arr[], int l, int h){
+    if(l < h){
+        int p = partition(arr, l, h);
+        quickSort(arr, l, p);
+        quickSort(arr, p+1, h);
+    }
+}
+int main(){
+    int arr[] = {8, 4, 7, 9, 3, 10, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    quickSort(arr, 0, n-1);
+    for(int x : arr)
+        cout<<x<<" ";
+    return 0;
+}
