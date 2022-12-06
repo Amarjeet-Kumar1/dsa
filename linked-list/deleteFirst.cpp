2424d18da3ccb8819b9ae349c704d49c83bd09c2
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-12-06 23:29:52 +0530

    delete head

---
+++ linked-list/deleteFirst.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node *next;
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+Node *delHead(Node *head){
+    if(head == NULL)
+        return NULL;
+    else {
+        Node *temp = head->next;
+        delete head;
+        return temp;
+    }
+}
+void printList(Node *head){
+    //assign head to curr
+    Node *curr = head;
+    //check if curr is after the last node which is null
+    while(curr != NULL){
+        cout<<curr->data<<" ";
+        //shift curr to next
+        curr = curr->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node *head = new Node(10);
+    head -> next = new Node(20);
+    head -> next -> next = new Node(30);
+    head -> next -> next -> next = new Node(40);
+    printList(head);
+    head = delHead(head);
+    printList(head);
+    return 0;
+}
