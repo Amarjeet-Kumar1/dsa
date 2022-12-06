f0472e2e71dbcef76c8bfb59f19dab49ff7531b3
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-12-06 22:21:06 +0530

    print nodes of ll

---
+++ linked-list/search-ll.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data; 
+    Node *next;
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+//search in linked list of given head and key
+int searchLL(Node *head, int x){
+    
+}
+
+int main(){
+    
+    return 0;
+}
