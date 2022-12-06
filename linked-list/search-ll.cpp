a494db623d9320dd12ceff1818c213e931263dc5
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-12-06 22:43:04 +0530

    search in ll

--- linked-list/search-ll.cpp
+++ linked-list/search-ll.cpp
@@ -12,10 +12,44 @@ struct Node{
 
 //search in linked list of given head and key
 int searchLL(Node *head, int x){
+    Node *curr = head;
+    int count = 1;
+    while(curr != NULL){
+        if(curr->data == x){
+            return count;
+        } else {
+            count++;
+            curr = curr->next;
+        }
+    }
+    return -1;
+}
+
+//recursively
+int rSearchLL(Node *head, int x){
+    if(head == NULL)
+        return -1;
+    
+    if(head -> data == x)
+        return 1;
+    else {
+    int res = rSearchLL(head->next, x);
+    if(res == -1)
+        return  -1;
+    else 
+        return (res+1);
+    }
     
 }
 
 int main(){
+    Node *head = new Node(10);
+    head ->next = new Node(5);
+    head ->next ->next = new Node(20);
+    head ->next ->next->next = new Node(15);
+    cout<<searchLL(head, 15)<<endl;
+    cout<<rSearchLL(head, 20);
     
+
     return 0;
 }
