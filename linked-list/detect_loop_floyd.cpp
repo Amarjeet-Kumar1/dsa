a0a6ebeabcb0c355654f23809196dab9f532bcaa
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-12-21 23:52:08 +0530

    floyd"s circle detection

---
+++ linked-list/detect_loop_floyd.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+bool detectLoop(Node *head){
+   Node *slow = head, *fast = head;
+   while(fast != NULL && fast->next != NULL){
+    slow = slow->next;
+    fast = fast->next->next;
+    if(slow == fast)
+        return true;
+   }
+   return false;
+}
+
+int main(){
+    Node *head = new Node(10);
+    head->next = new Node (20);
+    head->next->next = new Node (30);
+    head->next->next->next = new Node (40);
+    head->next->next->next->next = head->next;
+
+    if(detectLoop(head)){
+        cout<<"Yes";
+    } else{
+        cout<<"NO";
+    }
+
+
+    return 0;
+}
